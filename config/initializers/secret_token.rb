d640184441b380e8876417cfd0760dcfb5e56082
dghollingsworth <dghollingsworth@gmail.com>
2014-08-07 22:16:43 -0400

    installed and tested out haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogur2::Application.config.secret_key_base = 'd4f0eb7e0bae9c079b9c4cd1281414e756a76941aca6581569e54d997e4f607d9f04cd56c8f0b129ac57b41d886ec6f8c8dfd1d0afd22a57fd9624aac5f0ec46'
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Photogur2::Application.routes.draw do
+  resources :pictures
+
+  root :to => "pictures#index"
+
+ # get 'pictures'=>'pictures#index'
+ # get 'pictures/:id'=> 'pictures#show', as: "picture"
+ # get 'pictures/new'=>'pictures#new'
+ # get 'pictures/:id'=>'pictures#show'
+
+ # get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+ # patch "pictures/:id" => "pictures#update"
+
+ # post 'pictures'=>"pictures#create"
+
+ # delete 'pictures/:id'=>'pictures#destroy', as: "delete_picture"
+
+end
