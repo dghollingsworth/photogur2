e20d88298a9c9d22d1459e568899003758354178
dghollingsworth <dghollingsworth@gmail.com>
2014-08-12 23:45:29 -0400

    converted pictures show, edit, new, index to haml

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,13 @@ Photogur2::Application.routes.draw do
 
   root :to => "pictures#index"
 
- # get 'pictures'=>'pictures#index'
- # get 'pictures/:id'=> 'pictures#show', as: "picture"
- # get 'pictures/new'=>'pictures#new'
- # get 'pictures/:id'=>'pictures#show'
-
- # get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
- # patch "pictures/:id" => "pictures#update"
-
- # post 'pictures'=>"pictures#create"
-
- # delete 'pictures/:id'=>'pictures#destroy', as: "delete_picture"
+	# get 'pictures'=>'pictures#index'
+	# get 'pictures/:id'=> 'pictures#show', as: "picture"
+	# get 'pictures/new'=>'pictures#new'
+	# get 'pictures/:id'=>'pictures#show'
+	# get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+	# patch "pictures/:id" => "pictures#update"
+	# post 'pictures'=>"pictures#create"
+	# delete 'pictures/:id'=>'pictures#destroy'
 
 end
